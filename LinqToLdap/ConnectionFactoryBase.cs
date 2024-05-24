5a2be8ebee454a4b1d2cddff07080b0eec113157
rklec <71322635+rklec@users.noreply.github.com>
2024-05-24 11:49:16 +0200

    Change string.IsNullOrEmpty to new syntax
    This will be problematic in .NET 6 or higher, as an argument is required and this is the new syntax. (Seems to work in your version, too.)
    
    https://learn.microsoft.com/en-us/dotnet/api/system.string.isnullorempty?view=net-8.0

--- LinqToLdap/ConnectionFactoryBase.cs
+++ LinqToLdap/ConnectionFactoryBase.cs
@@ -1,4 +1,4 @@
-﻿using LinqToLdap.Logging;
+using LinqToLdap.Logging;
 using System;
 using System.DirectoryServices.Protocols;
 using System.Net;
@@ -41,7 +41,7 @@ namespace LinqToLdap
         /// <param name="serverName"></param>
         protected ConnectionFactoryBase(string serverName)
         {
-            if (serverName.IsNullOrEmpty()) throw new ArgumentNullException("serverName");
+            if (string.IsNullOrEmpty(serverName)) throw new ArgumentNullException("serverName");
 
             if (serverName.StartsWith("ldap://", StringComparison.OrdinalIgnoreCase))
             {
